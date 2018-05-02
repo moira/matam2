1e4d58e9d8f8adad5dfcee4e36fc662a6ccf24dd
Moira <moira@yandex.ru>
2018-05-02 20:22:40 +0300

    style

--- team.c
+++ team.c
@@ -1,29 +1,38 @@
 #include "team.h"
 
-static const char* StringDuplicate(const char* str);
+static const char* StringDuplicate(char* str);
 
 typedef struct team {
-	const char* TeamName;
-	Driver FirstDriver;
-	Driver SecondDriver;
+	const char* team_name;
+	Driver first_driver;
+	Driver second_driver;
+	Driver best_driver;
 	int points;
 } *Team;
 
 Team TeamCreate(TeamStatus* status, char* name) {
 	Team team = malloc(sizeof(*team));
-	if (!team) {
-		TeamStatus = MEMORY_ERROR;
+	if (team == NULL) {
+		if (status != NULL) {
+			*status = MEMORY_ERROR;
+		}
 		return NULL;
 	}
-	if (name == NULL) {
-		TeamDestroy(team);
+	if (name == NULL) { //https://moodle.technion.ac.il/mod/hsuforum/discuss.php?d=189
+		free(team);
+		if (status != NULL) {
+			*status = MEMORY_ERROR;
+		}
 		return NULL;
 	}
 	team->name = StringDuplicate(name);
 	team->FirstDriver = NULL;
 	team->SecondDriver = NULL;
-	team->points = 0; //WRONG
-	status = STATUS_OK;
+	team->BestDriver = NULL;
+	team->points = 0;
+	if (status != NULL) {
+		*status = STATUS_OK;
+	}
 	return team;
 }
 
@@ -63,32 +72,37 @@ Driver TeamGetDriver(Team team, DriverNumber driver_number) {
 	}
 }
 
-// int TeamGetPoints(Team team, TeamStatus *status) {
-// 	if (team == NULL) {
-// 		status = NULL;
-// 	} else {
-// 		int points = 0;
-// 		if (team->FirstDriver != NULL) {
-// 			points += FirstDriver->points;
-// 			if (team->SecondDriver != NULL) {
-// 				points += SecondDriver->points;
-// 			}
-// 		}
-// 		status = STATUS_OK;
-// 		return points;
-// 	}
-// }
+int TeamGetPoints(Team team, TeamStatus *status) {
+	if (team == NULL) {
+		if (status != NULL) {
+			*status = TEAM_NULL_PTR;
+		}
+		return 0;
+	} else {
+		int points = 0;
+		if (team->FirstDriver != NULL) {
+			points += FirstDriver->points;
+			if (team->SecondDriver != NULL) {
+				points += SecondDriver->points;
+			}
+		}
+		if (status != NULL) {
+			*status = STATUS_OK;
+		}
+		return points;
+	}
+}
 
 void TeamDestroy(Team team) {
-	free(team->FirstDriver);
-	free(team->SecondDriver);
+	free(team->first_driver);
+	free(team->second_driver);
+	free(team->best_driver);
 	free(team->name);
 	free(team);
 }
 
-static const char* StringDuplicate(const char* str) {
+static const char* StringDuplicate(char* str) {
 	char* copy = malloc(strlen(str) + 1);
 	return copy ? strcpy(copy, str) : NULL;
 }
-
 
